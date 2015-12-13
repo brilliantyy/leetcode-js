aa6e8c5134edc878bcdb5c6dd5f3b4eacfae88ae
Yoga <yujia6366@163.com>
2015-12-13 22:43:09 +0800

    Update

---
+++ Search a 2D Matrix.js
@@ -0,0 +1,47 @@
+@@ -1,46 +0,0 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+    
+    var start = 0;
+    var end = matrix.length - 1;
+    
+    while (start <= end) {
+        
+        var mid = Math.floor((start + end) / 2);
+        
+        if (matrix[mid][0] == target) {
+            return true;
+        } else if (matrix[mid][0] > target) {
+            end = mid - 1;
+        } else {
+            start = mid + 1;
+        }
+    }
+    
+    if (end == -1) {
+        return false;
+    } else {
+        var row = end;
+        start = 0;
+        end = matrix[row].length - 1;
+        
+        while (start <= end) {
+            
+            mid = Math.floor((start + end) / 2);
+            
+            if (matrix[row][mid] == target) {
+                return true;
+            } else if (matrix[row][mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return false;
+    }
+    
+};
